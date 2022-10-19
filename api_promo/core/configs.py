fa87143ca23aebcb7107ce790a333974a3c30ff6
Leandro Ferreira <leandroferreira281@gmail.com>
2022-10-19 10:01:22 -0300

    alter bd

--- api_promo/core/configs.py
+++ api_promo/core/configs.py
@@ -3,11 +3,11 @@ from typing import List
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
-
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
     #DB_URL: str = 'postgresql+asyncpg://postgres:1702@localhost:5432/promocao'
-    DB_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/promo'
+    #DB_URL: str = 'postgresql+asyncpg://admin:admin@localhost:5432/promo'
+    DB_URL: str = 'postgresql+asyncpg://postgres:589916@191.253.23.111:3152/promocao'
     DBBaseModel = declarative_base()
 
     JWT_SECRET: str = '81ZONkehu3qVaSKG0iAXrNAW97eMu62VobhRyEIeV3M'
